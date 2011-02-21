59f1ea534fab2827d502085e4bc4a108286066d5
bbot <github@bbot.org>
2011-02-21 06:43:52 -0800

    Added main loop to bar.sh

--- bar.sh
+++ bar.sh
@@ -18,11 +18,11 @@
 #
 # --------------------------------------------------------------------
 #
-# The first time you run bar.sh, it creates the HTML file, generates the navigation bar, adds the creation time, and adds it to the git repo. (git add then git commit)
+# The first time you run bar.sh, it creates the HTML file, generates the navigation bar, adds the creation time, and adds it to the git repo.
 #
 # Usage: ./bar.sh foo/baz.html
 #
-# The second time you run it, it checks to see if the file has been modified, and if it has, updates the modified time, runs HTML tidy to check for missing tags, GPG signs it with the specified key, runs a git commit, then pushes it to the remote repo. Alternatively, it can scp it to a remote server, if your production pipeline doesn't use git.
+# The second time you run it, it checks to see if the file has been modified, and if it has, updates the modified time, runs HTML tidy to check for missing tags, GPG signs it with the default key, runs a git commit, then pushes it to the remote repo. Alternatively, it can scp it to a remote server, if your production pipeline doesn't use git.
 #
 # bar.sh can be run on a directory, in which case, it recursively searches the directory tree for modified files, publishing them as it finds them.
 #
@@ -34,9 +34,11 @@
 #
 # bar.sh outputs valid HTML5, and generates well-formed semantic HTML, which, to a first approximation, nobody at all cares about. But this is my toy CMS, and I'll do what I want.
 #
-# bar.sh does not work out of the box. Everything in ALLCAPS is a site-specific setting, and should be changed by the end-user.
+# bar.sh does not work out of the box. Everything in [ALLCAPS SQUARE BRACKETS] is a site-specific setting, and should be changed by the end-user.
 
 FILE=$1
+# We stick the commit message inside strong quotes so you can use arbitrary text.
+COMMIT_MESSAGE=\'$2\'
 
 if [ -z "$FILE" ]
    then
@@ -44,9 +46,32 @@ echo "[!] This script needs a HTML file or a directory as an input."
    exit 1
 fi
 
+# Checks to see if the file exists, and if it doesn't, makes it.
 if [ ! -f "$FILE" ]
    then
-echo "[!] File does not exist."
+    make page
    exit 1
 fi
 
+# Main loop.
+
+for (( i=1; i <= $NUMBER_OF_FILES; i=i+1 ))
+do
+    # Validate HTML, and error out if it fails.
+    if [ foobar ]
+	then
+	tidy $FILE
+	exit 1
+    fi
+
+    # Now that we know the HTML is good, we can sign it...
+    gpg --clearsign $FILE
+
+    # ...and add it to git.
+    git add "$FILE".asc
+    git commit -a -m $COMMIT_MESSAGE
+    git push origin master
+
+    # Or SCP it, if no git.
+    #scp "$FILE"* [USER]@[REMOTE SERVER]:[PATH TO WWWROOT]
+done
