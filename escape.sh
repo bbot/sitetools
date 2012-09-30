634ec5f7502f4c44a0ccfea7b539caedffa822f9
bbot <bbot@magnesium.(none)>
2012-09-30 04:05:05 -0700

    Added sitetools.sh

---
+++ escape.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# escape.sh - Escapes angle brackets in text files
+#
+# Turns angle brackets into &lt; and &gt; HTML entities.
+# With -irc, replaces the first 8 columns (the timestamp) with an 
+# opening angle bracket, using an ugly hack.
+#
+# This is free and unencumbered software released into the public domain.
+
+if [[ $* == *--irc* ]]
+then
+    sed -i 's/>/\&gt;/g' $2
+    sed -i 's/^......../\&gt;/g' $2
+else
+    sed -i 's/</\&lt;/g' $1
+    sed -i 's/>/\&gt;/g' $1
+fi
